e79df2778b8b3a2e6aa91719dbdc1b0d633f1a0f
shettychetana <chetanashetty22@gmail.com>
2024-11-23 07:13:01 +0530

    api result chec1k

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FlightSearchForm from './components/FlightSearchForm';
 import FlightResults from './components/FlightResults';
 import './App.css';
+import Header from './components/Header';
 
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<FlightSearchForm />} />
-                <Route path="/results" element={<FlightResults />} />
-            </Routes>
-        </Router>
+        <div>
+            <Header/>
+            <Router>
+
+<Routes>
+    <Route path="/" element={<FlightSearchForm />} />
+    <Route path="/results" element={<FlightResults />} />
+</Routes>
+</Router>
+        </div>
+        
     );
 };
 
--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -2,10 +2,41 @@
 import React from "react";
 import "./../styles/Header.css";
 
-const Header = () => (
-  <header className="header">
-    
-  </header>
-);
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        {/* <img
+          src="/images/logo.jpeg"
+          alt="TravUnited Logo"
+          className="header-logo"
+        /> */}
+        <h1 className="header-title">TRAVUNITED</h1>
+        <p className="header-subtitle">LIVE LOVE EXPLORE</p>
+      </div>
+      <nav className="header-nav">
+        <a href="#flights" className="nav-link">FLIGHTS</a>
+        <a href="#visa" className="nav-link">VISA</a>
+        <a href="#hotels" className="nav-link">HOTELS</a>
+        <a href="#holidays" className="nav-link">HOLIDAYS</a>
+      </nav>
+      <div className="header-right">
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <i className="social-icon fab fa-instagram"></i>
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <i className="social-icon fab fa-facebook-f"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <i className="social-icon fab fa-twitter"></i>
+        </a>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <i className="social-icon fab fa-youtube"></i>
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
